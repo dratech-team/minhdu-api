c8b7a0ee97fb14d48cb2c3dbedc3246f823c663a
Long Trần (KMASoft) <tranlong.kma@gmail.com>
2021-04-06 00:05:31 +0700

    fix: import

--- src/api/salary/salary.controller.ts
+++ src/api/salary/salary.controller.ts
@@ -1,14 +1,14 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { SalaryService } from "./salary.service";
 import { CreateSalaryDto } from "./dto/create-salary.dto";
-import { SalaryInterface } from "./interfaces/salary.interface";
+import { Salary } from "./schemas/salary.schema";
 
 @Controller("v1/salary")
 export class SalaryController {
-  constructor(private readonly salaryService: SalaryService) {}
+  constructor(private readonly salaryService: SalaryService) { }
 
   @Post()
-  create(@Body() createSalaryDto: CreateSalaryDto): Promise<SalaryInterface> {
+  create(@Body() createSalaryDto: CreateSalaryDto): Promise<Salary> {
     return this.salaryService.create(createSalaryDto);
   }
 }
