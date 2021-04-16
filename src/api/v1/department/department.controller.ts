1eba856646c9a4d134a2de3ef50aaa5557644b8f
Trần Long (Admin) <tranlong.kma@gmail.com>
2021-04-16 22:05:47 +0700

    feat(department): crud

---
+++ src/api/v1/department/department.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from "@nestjs/common";
+import {BaseController} from "../../../core/crud-base/base-controller";
+import {Department} from "./schema/department.schema";
+import {DepartmentService} from "./department.service";
+import {CreateDepartmentDto} from "./dto/create-department.dto";
+import {ObjectId} from "mongodb";
+import {CorePaginateResult} from "../../../core/interfaces/pagination";
+import {UpdateDepartmentDto} from "./dto/update-department.dto";
+
+@Controller("v1/department")
+export class DepartmentController extends BaseController<Department> {
+  constructor(private readonly service: DepartmentService) {
+    super(service);
+  }
+
+  @Post()
+  async create(
+    @Body() body: CreateDepartmentDto,
+    ...args
+  ): Promise<Department> {
+    return super.create(body, ...args);
+  }
+
+  @Get(":id")
+  async findById(@Param("id") id: ObjectId, ...args): Promise<Department> {
+    return super.findById(id, ...args);
+  }
+
+  @Get()
+  async findAll(
+    @Param("page") page: number,
+    @Param("limit") limit: number,
+    ...args
+  ): Promise<CorePaginateResult<Department>> {
+    return super.findAll(page, limit, ...args);
+  }
+
+  @Put(":id")
+  async update(
+    @Body() updates: UpdateDepartmentDto,
+    id: ObjectId,
+    ...args
+  ): Promise<Department> {
+    return super.update(updates, id, ...args);
+  }
+
+  @Delete(":id")
+  async delete(id: ObjectId, ...args): Promise<void> {
+    return super.delete(id, ...args);
+  }
+}
---
+++ src/api/v1/department/department.service.ts
@@ -0,0 +1,48 @@
+import {Injectable} from "@nestjs/common";
+import {BaseService} from "../../../core/crud-base/base.service";
+import {Model} from "mongoose";
+import {Department, DepartmentDocument} from "./schema/department.schema";
+import {InjectModel} from "@nestjs/mongoose";
+import {ModelName} from "../../../common/constant/database.constant";
+import {CreateDepartmentDto} from "./dto/create-department.dto";
+import {ObjectId} from "mongodb";
+import {PaginatorOptions} from "../../../core/crud-base/interface/pagination.interface";
+import {CorePaginateResult} from "../../../core/interfaces/pagination";
+import {UpdateDepartmentDto} from "./dto/update-department.dto";
+
+@Injectable()
+export class DepartmentService extends BaseService<DepartmentDocument> {
+  constructor(
+    @InjectModel(ModelName.DEPARTMENT)
+    private readonly departmentModel: Model<DepartmentDocument>
+  ) {
+    super(departmentModel);
+  }
+
+  async create(body: CreateDepartmentDto, ...args): Promise<Department> {
+    return super.create(body, ...args);
+  }
+
+  async findOne(id: ObjectId, ...args): Promise<Department> {
+    return super.findOne(id, ...args);
+  }
+
+  async findAll(
+    paginateOpts?: PaginatorOptions,
+    ...args
+  ): Promise<CorePaginateResult<Department>> {
+    return super.findAll(paginateOpts, ...args);
+  }
+
+  async update(
+    id: ObjectId,
+    updates: UpdateDepartmentDto,
+    ...args
+  ): Promise<Department> {
+    return super.update(id, updates, ...args);
+  }
+
+  async delete(id: ObjectId, ...args): Promise<void> {
+    return super.delete(id, ...args);
+  }
+}
