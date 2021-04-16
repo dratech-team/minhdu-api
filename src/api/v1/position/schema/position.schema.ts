63097ff86eb99b36d91bed8effa7410364fdc31d
Trần Long (Admin) <tranlong.kma@gmail.com>
2021-04-16 22:08:32 +0700

    ci: ref position -> department

--- src/api/v1/position/schema/position.schema.ts
+++ src/api/v1/position/schema/position.schema.ts
@@ -1,8 +1,8 @@
-import { Document, Types } from "mongoose";
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { UserType } from "../../../../core/constants/role-type.constant";
-import { User } from "../../user/schema/user.schema";
-import { BaseDocument } from "../../../../core/schema/base.schema";
+import {Document} from "mongoose";
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {BaseDocument} from "../../../../core/schema/base.schema";
+import {Department} from "../../department/schema/department.schema";
+import {ObjectId} from "mongodb";
 
 export type PositionDocument = Position & Document;
 
@@ -14,11 +14,8 @@ export class Position extends BaseDocument {
   @Prop()
   wordDay: number;
 
-  @Prop()
-  userType: UserType;
-
-  @Prop({ type: Types.ObjectId, ref: "User" })
-  userId: User;
+  @Prop({type: ObjectId, ref: "Department"})
+  department: Department;
 }
 
 export const PositionSchema = SchemaFactory.createForClass(Position);
