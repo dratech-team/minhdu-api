f55b60691da75b380d5e60d8d4519a49bd9aca51
Tran Long (Admin) <tranlong.kma@gmail.com>
2021-09-04 13:59:58 +0700

    doing

--- src/api/v2/route/route.service.ts
+++ src/api/v2/route/route.service.ts
@@ -1,77 +1,93 @@
-import {Injectable,} from '@nestjs/common';
-import {CreateRouteDto} from './dto/create-route.dto';
-import {UpdateRouteDto} from './dto/update-route.dto';
+import {Injectable} from "@nestjs/common";
+import {CreateRouteDto} from "./dto/create-route.dto";
+import {UpdateRouteDto} from "./dto/update-route.dto";
 import {RouteRepository} from "./route.repository";
 import {ExportService} from "../../../core/services/export.service";
-import {Response} from 'express';
+import {Response} from "express";
+import {Route} from ".prisma/client";
 
 @Injectable()
 export class RouteService {
-    constructor(private readonly repository: RouteRepository, private readonly exportService: ExportService) {
+  constructor(
+    private readonly repository: RouteRepository,
+    private readonly exportService: ExportService
+  ) {
+  }
+
+  async create(body: CreateRouteDto) {
+    return await this.repository.create(body);
+  }
+
+  async findAll(
+    skip: number,
+    take: number,
+    name: string,
+    startedAt: Date,
+    endedAt: Date,
+    driver: string,
+    bsx: string
+  ) {
+    if (startedAt) {
+      startedAt = new Date(startedAt);
     }
 
-    async create(body: CreateRouteDto) {
-        return await this.repository.create(body);
+    if (endedAt) {
+      endedAt = new Date(endedAt);
     }
 
-    async findAll(
-        skip: number,
-        take: number,
-        name: string,
-        startedAt: Date,
-        endedAt: Date,
-        driver: string,
-        bsx: string,
-    ) {
-        if (startedAt) {
-            startedAt = new Date(startedAt);
-        }
+    return await this.repository.findAll(skip, take, {
+      name,
+      startedAt,
+      endedAt,
+      driver,
+      bsx,
+    });
+  }
 
-        if (endedAt) {
-            endedAt = new Date(endedAt);
-        }
+  async findOne(id: number) {
+    return await this.repository.findOne(id);
+  }
 
-        return await this.repository.findAll(skip, take, {name, startedAt, endedAt, driver, bsx});
-    }
+  async update(id: number, updates: UpdateRouteDto) {
+    return await this.repository.update(id, updates);
+  }
 
-    async findOne(id: number) {
-        return await this.repository.findOne(id);
-    }
+  async remove(id: number) {
+    return await this.repository.remove(id);
+  }
 
-    async update(id: number, updates: UpdateRouteDto) {
-        return await this.repository.update(id, updates);
-    }
+  async export(response?: Response, search?: Partial<CreateRouteDto>) {
+    const data = await this.repository.finds(search);
 
-    async remove(id: number) {
-        return await this.repository.remove(id);
-    }
-
-    async export(response?: Response, search?: Partial<CreateRouteDto>) {
-        const data = await this.repository.finds(search);
-
-        const res = this.exportService.toExcel(response, {
-            excel: {
-                /// TODO: Truyền vào title
-                title: "Danh sách tuyến đường XXXX",
-                customHeaders: [
-                    {header: "Tên tuyến đường", key: "name"},
-                    {header: "Ngày khởi hành", key: "startedAt", numFmt: "dd/MM/yyyy"},
-                    {header: "Ngày kết thúc", key: "endedAt"},
-                    {header: "Nhà xe", key: "garage"},
-                    {header: "Tên tài xế", key: "driver"},
-                    {header: "Biển số xe", key: "bsx"},
-                ],
-                name: "data.xlsx",
-                data: data.map(e => ({
-                    name: e.name,
-                    startedAt: e.startedAt,
-                    endedAt: e.endedAt,
-                    garage: e.garage,
-                    driver: e.driver,
-                    bsx: e.bsx
-                })),
-            }
-        }, 200);
-        console.log(res);
-    }
+    const res = this.exportService.toExcel<Route>(
+      response,
+      {
+        excel: {
+          /// TODO: Truyền vào title
+          title: "Danh sách tuyến đường XXXX",
+          customHeaders: [
+            {header: "Tên tuyến đường", key: "name"},
+            {
+              header: "Ngày khởi hành",
+              key: "startedAt"
+            },
+            {header: "Ngày kết thúc", key: "endedAt"},
+            {header: "Nhà xe", key: "garage"},
+            {header: "Tên tài xế", key: "driver"},
+            {header: "Biển số xe", key: "bsx"},
+          ],
+          name: "data.xlsx",
+          data: data.map((e) => ({
+            name: e.name,
+            startedAt: e.startedAt,
+            endedAt: e.endedAt,
+            garage: e.garage,
+            driver: e.driver,
+            bsx: e.bsx,
+          })),
+        },
+      },
+      200
+    );
+  }
 }
--- src/core/services/export.service.ts
+++ src/core/services/export.service.ts
@@ -3,189 +3,84 @@ import {optionalRequire} from "@nestjs/core/helpers/optional-require";
 import {Response} from "express";
 import {CoreResponse} from "../interfaces/coreResponse.interface";
 import * as ExcelJS from 'exceljs';
-import {Style} from "exceljs";
+import {Column, Style, Worksheet} from 'exceljs';
 
 const moment = optionalRequire("moment");
 
 @Injectable()
 export class ExportService {
 
-    toExcel(
-        response: Response,
-        result: CoreResponse,
-        respStatusCode: number
-    ): any {
+  toExcel<T>(
+    response: Response,
+    result: CoreResponse,
+    respStatusCode: number
+  ): any {
+    const workbook = new ExcelJS.Workbook();
+    workbook.creator = 'Me';
+    workbook.lastModifiedBy = 'Her';
+    workbook.created = new Date(1985, 8, 30);
+    workbook.modified = new Date();
+    workbook.lastPrinted = new Date(2016, 9, 27);
 
+    const worksheet = workbook.addWorksheet(result.excel.title);
 
-        const workbook = new ExcelJS.Workbook();
-        workbook.creator = 'Me';
-        workbook.lastModifiedBy = 'Her';
-        workbook.created = new Date(1985, 8, 30);
-        workbook.modified = new Date();
-        workbook.lastPrinted = new Date(2016, 9, 27);
-
-        const worksheet = workbook.addWorksheet(result.excel.title);
-
-        worksheet.columns = result.excel.customHeaders.map((data) => {
-            return Object.assign(
-                data,
-                {
-                    width: data.header.length + 5,
-                    style: {
-                        shrinkToFit: true,
-                        alignment: {horizontal: 'center'},
-                        wrapText: true,
-                    } as Partial<Style>
-                }
-            );
-        });
-
-        console.log(worksheet.columns)
-        result.excel.data.map((data) => {
-            worksheet.addRow(data);
-        });
-
-        const buf = workbook.xlsx.writeFile(result.excel.name);
-
-        response.header(
-            "Content-Disposition",
-            `attachment; filename=${result.excel.name} (${moment().format(
-                "DD-MM-YYYY"
-            )}).xlsx`
-        );
-        response.header(
-            "Content-Type",
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        );
-        response.status(respStatusCode).send(buf);
-    }
-
-
-    // toExcel(
-    //   response: Response,
-    //   result: CoreResponse,
-    //   respStatusCode: number
-    // ): any {
-    //   /*
-    //   * title: Tiêu đề của sheet
-    //   * header: tiêu đề của table
-    //   * data: Dữ liệu dạng json được truyền vào
-    //   * */
-
-    //   /*
-    //  * Get data đc truyền vào dưới dạng json
-    //  */
-    //   const excelData = result.excel.data;
-
-    //   const workbook = new ExcelJS.Workbook();
-
-    //   /*
-    //   * Data size của mỗi ô đc tính theo length của header
-    //   */
-    //   const dataSize = excelData ? Object.keys(excelData[0]).length : 0;
-
-    //   const wb = XLSX.utils.book_new();
-    //   let ws: WorkSheet;
-
-    //   /*
-    //   * Gộp các ô thành tiêu đề của sheet
-    //   * s: start
-    //   * r: row
-    //   * c: column
-    //   * e: end
-    //   */
-
-    //   if (result.excel.customHeaders) {
-
-    //     ///TODO: Thêm tiêu đề từ col 1 - 10 của row 1
-    //     // ws = XLSX.utils.cell_add_comment(wb, ws, "asdasd");
-
-    //     ws["!merges"] = [
-    //       {s: {r: 1, c: 0}, e: {r: 1, c: 10}}
-    //     ];
-
-    //     /*
-    //      * Append headers & data
-    //      */
-    //     ws = XLSX.utils.sheet_add_aoa(wb, [result.excel.customHeaders], {origin: "A5"});
-    //     XLSX.utils.sheet_add_json(ws, excelData, {
-    //       origin: "A6",
-    //       skipHeader: true,
-    //     });
-    //   } else {
-    //     ws = XLSX.utils.json_to_sheet(excelData);
-    //   }
-
-    //   // Auto filter
-    //   ws["!autofilter"] = {
-    //     ref: `A1:${ExportService.getExcelColumn(dataSize)}1`,
-    //   };
-    //   // Auto fit
-    //   ExportService.autoFitColumns(excelData, ws, result.excel.customHeaders);
-
-    //   // Append data to sheet
-    //   XLSX.utils.book_append_sheet(wb, ws, "Sheet 1");
-
-    //   /* generate buffer */
-    //   const buf = XLSX.writeFile(wb, result.excel.name, {type: "file", bookType: "xlsx"});
-    //   response.header(
-    //     "Content-Disposition",
-    //     `attachment; filename=${result.excel.name} (${moment().format(
-    //       "DD-MM-YYYY"
-    //     )}).xlsx`
-    //   );
-    //   response.header(
-    //     "Content-Type",
-    //     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    //   );
-    //   response.status(respStatusCode).send(buf);
-    // }
-
-    // This function can be bad performance if the excel data is big
-
-    private static autoFitColumns(
-        json: any[],
-        worksheet: any,
-        header?: string[]
-    ): void {
-        const jsonKeys = header ? header : Object.keys(json[0]);
-        const objectMaxLength = [];
-
-        for (let i = 0; i < json.length; i++) {
-            const value = json[i];
-            for (let j = 0; j < jsonKeys.length; j++) {
-                if (typeof value[jsonKeys[j]] == "number") {
-                    objectMaxLength[j] = 10;
-                } else {
-                    const l = value[jsonKeys[j]] ? value[jsonKeys[j]].length : 0;
-                    objectMaxLength[j] = objectMaxLength[j] >= l ? objectMaxLength[j] : l;
-                }
-            }
-
-            const key = jsonKeys;
-            for (let j = 0; j < key.length; j++) {
-                objectMaxLength[j] =
-                    objectMaxLength[j] >= key[j].length
-                        ? objectMaxLength[j]
-                        : key[j].length;
-            }
+    const header = this.autoFitColumnsHeader<T>(result.excel.data, worksheet, result.excel.customHeaders).map((data) => {
+      return Object.assign(
+        data,
+        {
+          style: {
+            alignment: {horizontal: 'center'},
+          }
         }
+      );
+    });
 
-        worksheet["!cols"] = objectMaxLength.map((w) => {
-            return {width: w};
-        });
+    // @ts-ignore
+    // worksheet.setColumnKey("A5", header);
+    worksheet.setColumnKey("A5", {header: header});
+    result.excel.data.map((data) => {
+      worksheet.insertRow(5, data);
+    });
+
+    const buf = workbook.xlsx.writeFile(result.excel.name);
+
+    response.header(
+      "Content-Disposition",
+      `attachment; filename=${result.excel.name} (${moment().format(
+        "DD-MM-YYYY"
+      )}).xlsx`
+    );
+    response.header(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    response.status(respStatusCode).send(buf);
+  }
+
+  private autoFitColumnsHeader<T>(
+    json: T[],
+    worksheet: Worksheet,
+    header?: Array<Partial<Column>>
+  ) {
+    const jsonKeys = header.map(e => e.key);
+    const objectMaxLength = [];
+
+    for (let i = 0; i < json.length; i++) {
+      const value = json[i];
+
+      for (let j = 0; j < jsonKeys.length; j++) {
+        const l = value[jsonKeys[j]]?.length ?? 0;
+        objectMaxLength[j] = objectMaxLength[j] >= l ? objectMaxLength[j] : l;
+      }
+
+      const key = header.map(e => e.header);
+      for (let j = 0; j < key.length; j++) {
+        objectMaxLength[j] =
+          objectMaxLength[j] >= key[j].length
+            ? objectMaxLength[j]
+            : key[j].length;
+      }
     }
-
-    private static getExcelColumn(index: number): string {
-        let colName = "",
-            dividend = Math.floor(Math.abs(index)),
-            rest;
-
-        while (dividend > 0) {
-            rest = (dividend - 1) % 26;
-            colName = String.fromCharCode(65 + rest) + colName;
-            dividend = parseInt(String((dividend - rest) / 26));
-        }
-        return colName;
-    }
+    return header.map((e, i) => (Object.assign(e, {width: objectMaxLength[i] + 5})));
+  }
 }
