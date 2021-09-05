380141b97237a3df26efe368d2d5b61b1b00c090
Tran Long (Admin) <tranlong.kma@gmail.com>
2021-09-05 13:17:49 +0700

    doing

--- src/core/services/export.service.ts
+++ src/core/services/export.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
-import { optionalRequire } from "@nestjs/core/helpers/optional-require";
-import { Response } from "express";
-import { InputExcel } from "../interfaces/coreResponse.interface";
+import {Injectable} from "@nestjs/common";
+import {optionalRequire} from "@nestjs/core/helpers/optional-require";
+import {Response} from "express";
+import {InputExcel} from "../interfaces/coreResponse.interface";
 import * as ExcelJS from "exceljs";
 
 const moment = optionalRequire("moment");
@@ -18,14 +18,54 @@ export class ExportService {
 
     const worksheet = workbook.addWorksheet(result.title);
 
+    /*
+    * Header
+    * */
     worksheet.mergeCells("A1", "J2");
-    worksheet.getCell("C1").value = "Bảng lương tháng";
+    worksheet.getCell("C1").value = result.title;
 
+    // alignment
+    worksheet.getCell("C1").style.alignment = {
+      horizontal: "center",
+      vertical: "middle",
+    };
+    // font
+    worksheet.getCell("C1").style.font = {
+      bold: true,
+      size: 24,
+    };
+
+    /*
+    * Title
+    * */
     worksheet.getRow(4).values = result.customHeaders;
+
+    // font
+    worksheet.getRow(4).font = {
+      bold: true,
+      outline: true,
+    };
+
+    // alignment
+    worksheet.getRow(4).alignment = {
+      horizontal: "center",
+      vertical: "middle",
+    };
+
+    // alignment
+    // for (let i = 0; i < result.customHeaders.length; i++) {
+    //   worksheet.getCell(alphabet[i] + i).style.fill = {
+    //     type: 'pattern',
+    //     pattern: 'darkGray',
+    //     fgColor: {argb: 'FFFFFF00'},
+    //     bgColor: {argb: 'FF0000FF'}
+    //   };
+    // }
 
     worksheet.columns = this.autoFitColumnsHeader(result);
 
     result.data.map((e) => {
+      console.log(alphabet);
       worksheet.addRow(e);
     });
 
@@ -56,7 +96,6 @@ export class ExportService {
         const l = value[jsonKeys[j]]?.length ?? 0;
         objectMaxLength[j] = objectMaxLength[j] >= l ? objectMaxLength[j] : l;
 
-        console.log(typeof value[jsonKeys[j]]);
         if (typeof value[jsonKeys[j]].getMonth === "function") {
           numFmt[j] = "dd/mm/yyyy";
         }
@@ -77,3 +116,5 @@ export class ExportService {
     });
   }
 }
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split(", ");
