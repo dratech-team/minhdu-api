03920ef686dea5ec9f1a0ef17af1b8fb9ea75f74
Tran Long (Admin) <tranlong.kma@gmail.com>
2021-09-06 20:50:31 +0700

    fix export

--- src/api/v2/route/route.controller.ts
+++ src/api/v2/route/route.controller.ts
@@ -9,13 +9,14 @@ import {
   Query,
   Res,
 } from "@nestjs/common";
-import { RouteService } from "./route.service";
-import { CreateRouteDto } from "./dto/create-route.dto";
-import { UpdateRouteDto } from "./dto/update-route.dto";
+import {RouteService} from "./route.service";
+import {CreateRouteDto} from "./dto/create-route.dto";
+import {UpdateRouteDto} from "./dto/update-route.dto";
 
 @Controller("v2/route")
 export class RouteController {
-  constructor(private readonly routeService: RouteService) {}
+  constructor(private readonly routeService: RouteService) {
+  }
 
   @Post()
   create(@Body() createRouteDto: CreateRouteDto) {
@@ -59,7 +60,14 @@ export class RouteController {
   }
 
   @Get("/export/print")
-  async print(@Res() res) {
-    return this.routeService.export(res);
+  async print(
+    @Res() res,
+    @Query("name") name: string,
+    @Query("startedAt") startedAt: Date,
+    @Query("endedAt") endedAt: Date,
+    @Query("driver") driver: string,
+    @Query("bsx") bsx: string
+  ) {
+    return this.routeService.export(res, {name, startedAt, endedAt, driver, bsx});
   }
 }
