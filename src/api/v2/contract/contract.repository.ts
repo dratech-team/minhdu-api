6a6ff10170e9871d24fe6d53be2dadad06b11aab
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-09-27 01:48:59 +0700

    fix add contract

--- src/api/v2/contract/contract.repository.ts
+++ src/api/v2/contract/contract.repository.ts
@@ -12,7 +12,18 @@ export class ContractRepository {
   }
 
   async create(body: CreateContractDto): Promise<Contract> {
-    return await this.prisma.contract.create({ data: body });
+    const employee = await this.prisma.employee.findUnique({
+      where: { id: body.employeeId },
+      include: { position: true },
+    });
+    return await this.prisma.contract.create({
+      data: {
+        employee: { connect: { id: body.employeeId } },
+        createdAt: body.createdAt,
+        expiredAt: body.expiredAt,
+        position: employee.position.name,
+      },
+    });
   }
 
   async findAll(): Promise<Contract[]> {
@@ -27,8 +38,8 @@ export class ContractRepository {
     return Promise.resolve(undefined);
   }
 
- async update(id: number, updates: any): Promise<Contract> {
-    return await this.prisma.contract.update({where: {id}, data: updates});
+  async update(id: number, updates: any): Promise<Contract> {
+    return await this.prisma.contract.update({ where: { id }, data: updates });
   }
 
   remove(id: number): void {}
