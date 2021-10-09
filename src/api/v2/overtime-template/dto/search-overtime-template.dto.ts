835397c96df17af862b3938732ba19fff0f4ebf3
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-10-09 14:02:19 +0700

    init repo in template overtime

--- src/api/v2/overtime-template/dto/search-overtime-template.dto.ts
+++ src/api/v2/overtime-template/dto/search-overtime-template.dto.ts
@@ -2,9 +2,9 @@ import { DatetimeUnit, SalaryType } from "@prisma/client";
 
 export interface SearchOvertimeTemplateDto {
   title: string;
+  branchId: number;
   positionId: number;
   // type: SalaryType;
   price: number;
-  department: string;
   unit: DatetimeUnit;
 }
