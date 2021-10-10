a5ebed3b5470cda28759312139d0583037ffb6ca
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-10-10 14:19:32 +0700

    fix ct1

--- src/common/constant/salary.constant.ts
+++ src/common/constant/salary.constant.ts
@@ -1,2 +1,7 @@
 export const TAX = 0.115;
+
+// rate làm việc ngoài ngày công chuẩn của khối chăn nuôi
 export const RATE_OUT_OF_WORK_DAY = 2;
+
+// Khối văn phòng ap dụng cho ngày lễ
+export const PAYSLIP_WORKDAY_HOLIDAY = 26;
