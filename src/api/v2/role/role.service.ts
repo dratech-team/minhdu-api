f87fd31109612c8ae816465fd6f2ca8cf6c792c2
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-10-17 16:45:59 +0700

    add aapp name

--- src/api/v2/role/role.service.ts
+++ src/api/v2/role/role.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@nestjs/common';
 import {CreateRoleDto} from './dto/create-role.dto';
 import {UpdateRoleDto} from './dto/update-role.dto';
 import {PrismaService} from "../../../prisma.service";
-import {Role} from "@prisma/client";
+import {AppEnum, Role} from "@prisma/client";
 
 @Injectable()
 export class RoleService {
@@ -34,13 +34,16 @@ const roles = [
   {
     name: "Nhân sự",
     role: Role.HUMAN_RESOURCE,
+    appName: AppEnum.HR
   },
   {
     name: "Kế toán trại",
     role: Role.CAMP_ACCOUNTING,
+    appName: AppEnum.HR
   },
   {
     name: "Quản lý trại",
     role: Role.CAMP_MANAGER,
+    appName: AppEnum.HR
   }
 ];
