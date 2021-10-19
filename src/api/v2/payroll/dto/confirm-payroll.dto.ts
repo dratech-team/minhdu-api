83ca14265592a8fce61193b0167380e700e52dc5
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-10-19 14:45:34 +0700

    del trnasform confirm

--- src/api/v2/payroll/dto/confirm-payroll.dto.ts
+++ src/api/v2/payroll/dto/confirm-payroll.dto.ts
@@ -3,7 +3,6 @@ import {IsDate, IsOptional} from "class-validator";
 
 export class ConfirmPayrollDto {
   @IsOptional()
-  @Transform((val) => new Date(val.value))
   @Type(() => Date)
   @IsDate()
   readonly datetime: Date;
