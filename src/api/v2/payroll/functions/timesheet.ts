0611c6b9340cfe04637b432aa130cf5700cd7823
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-10-20 10:08:19 +0700

    fix absent tick

--- src/api/v2/payroll/functions/timesheet.ts
+++ src/api/v2/payroll/functions/timesheet.ts
@@ -1,13 +1,13 @@
 import {Salary, SalaryType} from "@prisma/client";
 import * as moment from "moment";
 import {firstDatetimeOfMonth, lastDatetimeOfMonth} from "../../../../utils/datetime.util";
-import {includesDatetime} from "../../../../common/utils/isEqual-datetime.util";
+import {includesDatetime, isEqualDatetime} from "../../../../common/utils/isEqual-datetime.util";
 import {PARTIAL_DAY} from "../../../../common/constant/datetime.constant";
 
 export const rageDaysInMonth = (datetime: Date) => {
   const range = [];
   const fromDate = moment(firstDatetimeOfMonth(datetime));
-  const toDate = moment(lastDatetimeOfMonth(datetime));
+  const toDate = moment(isEqualDatetime(datetime, new Date(), "month") ? new Date : lastDatetimeOfMonth(datetime));
   const diff = toDate.diff(fromDate, "days") + 1;
   for (let i = 0; i < diff; i++) {
     range.push(moment(firstDatetimeOfMonth(datetime)).add(i, "days"));
@@ -42,8 +42,9 @@ export const timesheet = (createdAt: Date, salaries: Salary[], isExport?: boolea
     }
   }
 
-  // range.find(datetime => )
+  /// FIXME: DEVELOP
+  // return {datime: range, total};
+  return range;
 
-  return {range, total};
 };
 
