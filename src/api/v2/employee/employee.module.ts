aa1c90a3b14cd6a8f8a5f8e8c9a794d08e4d8855
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-11-06 10:30:46 +0700

    del branch module in employee module

--- src/api/v2/employee/employee.module.ts
+++ src/api/v2/employee/employee.module.ts
@@ -1,23 +1,19 @@
-import { Module } from '@nestjs/common';
-import { BranchModule } from "../../../common/branches/branch/branch.module";
-import { BranchRepository } from "../../../common/branches/branch/branch.repository";
-import { ConfigModule } from "../../../core/config/config.module";
-import { PrismaService } from "../../../prisma.service";
-import { EmployeeController } from './employee.controller';
-import { EmployeeRepository } from "./employee.repository";
-import { EmployeeService } from "./employee.service";
+import {Module} from '@nestjs/common';
+import {ConfigModule} from "../../../core/config/config.module";
+import {PrismaService} from "../../../prisma.service";
+import {EmployeeController} from './employee.controller';
+import {EmployeeRepository} from "./employee.repository";
+import {EmployeeService} from "./employee.service";
 
 @Module({
   imports: [
     ConfigModule,
-    BranchModule,
   ],
   controllers: [EmployeeController],
   providers: [
     PrismaService,
     EmployeeService,
     EmployeeRepository,
-    BranchRepository,
   ],
   exports: [EmployeeService]
 })
