01bd767ab9a9f7709f2e871f22a4c90b4f9ad845
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-11-16 12:09:34 +0700

    change endpoint

--- src/api/v2/salary/salary.controller.ts
+++ src/api/v2/salary/salary.controller.ts
@@ -52,7 +52,7 @@ export class SalaryController {
   }
 
   @Roles(RoleEnum.CAMP_ACCOUNTING, RoleEnum.HUMAN_RESOURCE)
-  @Patch("salaries/employees")
+  @Patch("salaries/ids")
   async updateMany(@ReqProfile() profile: ProfileEntity, @Param("id") id: number, @Body() updateSalaryDto: UpdateManySalaryDto) {
     return await this.salaryService.updateMany(profile, +id, updateSalaryDto);
   }
