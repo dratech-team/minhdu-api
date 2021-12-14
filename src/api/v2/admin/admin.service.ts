a59b753dc5a5a63f704b88ef1cd53409b570c2b8
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-12-14 15:25:11 +0700

    hr admin

--- src/api/v2/admin/admin.service.ts
+++ src/api/v2/admin/admin.service.ts
@@ -5,6 +5,7 @@ import {PrismaService} from "../../../prisma.service";
 import {SearchAdminDto} from "./dto/search-admin.dto";
 import {TypeEnum} from "./entities/type.enum";
 import {firstDatetime, lastDatetime} from "../../../utils/datetime.util";
+import * as moment from "moment";
 
 @Injectable()
 export class AdminService {
@@ -17,36 +18,74 @@ export class AdminService {
 
   async findAll(take: number, skip: number, search: Partial<SearchAdminDto>) {
     try {
-      const datetimes = await this.prisma.payroll.groupBy({
+      const datetimes = (await this.prisma.payroll.groupBy({
         by: ['createdAt'],
         orderBy: {
           createdAt: "asc"
         }
-      });
-
-      const hr = await Promise.all(datetimes.map(async e => {
-        const payrolls = await this.prisma.payroll.findMany({
-          take: take || undefined,
-          skip: skip || undefined,
-          where: {
-            createdAt: search.type === TypeEnum.YEAR ? {
-              gte: firstDatetime(e.createdAt, "years"),
-              lte: lastDatetime(e.createdAt, "years"),
-            } : search.type === TypeEnum.MONTH ? {
-
-            } : {}
-          },
-          select: {
-            total: true
-          }
-        });
-
-        return {
-          datetime: e.createdAt,
-          total: payrolls.filter(payroll => payroll.total).map(payroll => payroll.total).reduce((a, b) => a + b, 0)
-        };
       }));
-      console.log(hr);
+
+      const yearsDiff = datetimes.reduce((acc, e) => {
+        if (acc.length === 0) {
+          return [...acc, e];
+        }
+        const year = moment(e.createdAt).year();
+        if (acc.some((d => moment(d.createdAt).year() === year))) {
+          return acc;
+        }
+        return [...acc, e];
+      }, []);
+
+      switch (search.type) {
+        case TypeEnum.YEAR: {
+          return await Promise.all(yearsDiff.map(async e => {
+            const payrolls = await this.prisma.payroll.findMany({
+              take: take || undefined,
+              skip: skip || undefined,
+              where: {
+                createdAt: {
+                  gte: firstDatetime(e.createdAt, "years"),
+                  lte: lastDatetime(e.createdAt, "years"),
+                }
+              },
+              select: {
+                total: true,
+              }
+            });
+
+            return {
+              name: "",
+              branch: "",
+              type: "",
+              datetime: e.createdAt,
+              total: payrolls.filter(payroll => payroll.total).map(payroll => payroll.total).reduce((a, b) => a + b, 0)
+            };
+          }));
+        }
+        case TypeEnum.MONTH: {
+          if (!search?.year) {
+            throw new BadRequestException('Nếu type là MONTH thì bắt buộc phải truyền year');
+          }
+          return await Promise.all([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(async month => {
+            const payrolls = await this.prisma.payroll.findMany({
+              where: {
+                createdAt: {
+                  gte: firstDatetime(new Date(`${search.year}-${month}-1`), "months"),
+                  lte: lastDatetime(new Date(`${search.year}-${month}-1`), "months"),
+                }
+              },
+              select: {
+                total: true
+              }
+            });
+
+            return {
+              month: month,
+              total: payrolls.filter(payroll => payroll.total).map(payroll => payroll.total).reduce((a, b) => a + b, 0)
+            };
+          }));
+        }
+      }
     } catch (e) {
       console.error(e);
       throw new BadRequestException(e);
--- src/api/v2/admin/dto/search-admin.dto.ts
+++ src/api/v2/admin/dto/search-admin.dto.ts
@@ -1,8 +1,14 @@
 import {TypeEnum} from "../entities/type.enum";
-import {IsEnum, IsNotEmpty} from "class-validator";
+import {IsEnum, IsNotEmpty, IsNumber, IsOptional} from "class-validator";
+import {Type} from "class-transformer";
 
 export class SearchAdminDto {
   @IsNotEmpty()
   @IsEnum(TypeEnum)
   readonly type: TypeEnum;
+
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  readonly year: number;
 }
