2a24e56b3be03ab07888f890a3b39cd10fb6efda
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-12-19 21:18:42 +0700

    add wrap text

--- src/api/v2/payroll/functions/convertArrayToString.ts
+++ src/api/v2/payroll/functions/convertArrayToString.ts
@@ -2,7 +2,7 @@ export function convertArrayToString<T>(items: T[]) {
   if (items.length > 1) {
     return items.reduce((a, b, i) => {
       if (i !== 0) {
-        return a + " + " + b;
+        return a + "\n" + b;
       }
       return a + b;
     }, '');
