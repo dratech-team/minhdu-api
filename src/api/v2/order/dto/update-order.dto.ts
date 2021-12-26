d51d59edef22b06854b301499f2c202b071671ed
Long Tran <90371694+LongTH20@users.noreply.github.com>
2021-12-26 15:22:36 +0700

    fix conflict

--- src/api/v2/order/dto/update-order.dto.ts
+++ src/api/v2/order/dto/update-order.dto.ts
@@ -1,10 +1,6 @@
 import {PartialType} from "@nestjs/mapped-types";
 import {CreateOrderDto} from "./create-order.dto";
-<<<<<<< HEAD
-import {IsBoolean, IsDate, IsNumber, IsOptional} from "class-validator";
-=======
 import {IsBoolean, IsDate, IsNumber, IsOptional, MaxDate} from "class-validator";
->>>>>>> 05d592b52b6e1e413a3f5140bdc7b3af4eb69ed1
 import {Type} from "class-transformer";
 
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {
@@ -22,9 +18,5 @@ export class UpdateOrderDto extends PartialType(CreateOrderDto) {
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
-<<<<<<< HEAD
   readonly total: number;
-=======
-  total?: number
->>>>>>> 05d592b52b6e1e413a3f5140bdc7b3af4eb69ed1
 }
