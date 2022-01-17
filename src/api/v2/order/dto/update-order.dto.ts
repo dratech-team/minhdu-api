518d92840fb98c196a821070381bbfbef4dedde9
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-01-17 11:02:13 +0700

    del max date

--- src/api/v2/order/dto/update-order.dto.ts
+++ src/api/v2/order/dto/update-order.dto.ts
@@ -6,7 +6,6 @@ import {Type} from "class-transformer";
 export class UpdateOrderDto extends PartialType(CreateOrderDto) {
   @IsOptional()
   @Type(() => Date)
-  @MaxDate(new Date(), {message: `Ngày tối đa được chọn là ngày hôm nay. Đơn hàng đã giao không phải là ngày của tương lai`})
   @IsDate()
   readonly deliveredAt: Date;
 
