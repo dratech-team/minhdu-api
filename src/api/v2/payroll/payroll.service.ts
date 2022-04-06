490e4be90d2ba499b76736cab2ca065ed9893ee3
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-04-07 01:19:32 +0700

    a

--- src/api/v2/payroll/payroll.service.ts
+++ src/api/v2/payroll/payroll.service.ts
@@ -511,17 +511,17 @@ export class PayrollService {
     // total: Ngày cuối cùng của tháng do mình quy định. Áp dụng đối với lương cố dịnh
     const confirmedAt = payroll.accConfirmedAt;
     if (!confirmedAt) {
-      if (payroll.isFlatSalary) {
-        total = 30;
+      // if (payroll.isFlatSalary) {
+      //   total = 30;
+      // } else {
+      if (isEqualDatetime(new Date(), payroll.createdAt, "month")) {
+        total = new Date().getDate() + 1 - payroll.createdAt.getDate();
+      } else if (moment(new Date()).isAfter(payroll.createdAt, "month")) {
+        total = lastDatetime(payroll.createdAt).getDate() + new Date().getDate();
       } else {
-        if (isEqualDatetime(new Date(), payroll.createdAt, "month")) {
-          total = new Date().getDate() + 1 - payroll.createdAt.getDate();
-        } else if (moment(new Date()).isAfter(payroll.createdAt, "month")) {
-          total = lastDatetime(payroll.createdAt).getDate() + new Date().getDate();
-        } else {
-          throw new BadRequestException("Phiếu lương chưa xác nhận có ngày nhỏ hơn ngày tạo phiếu lương");
-        }
+        throw new BadRequestException("Phiếu lương chưa xác nhận có ngày nhỏ hơn ngày tạo phiếu lương");
       }
+      // }
     } else {
       if (payroll.isFlatSalary) {
         total = 30;
