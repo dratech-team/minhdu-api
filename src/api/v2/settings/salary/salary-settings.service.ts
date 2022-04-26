a13f6196faabb5888dbc360c26d0a674dea4c9da
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-04-26 08:57:04 +0700

    fix(salary): check null add, update

--- src/api/v2/settings/salary/salary-settings.service.ts
+++ src/api/v2/settings/salary/salary-settings.service.ts
@@ -12,7 +12,7 @@ export class SalarySettingsService {
   }
 
   async create(body: CreateSalarySettingsDto) {
-    const types = body.totalOf.includes(SalaryType.BASIC) ? [SalaryType.BASIC, SalaryType.BASIC_INSURANCE] : body.totalOf;
+    const types = body?.totalOf?.length && body.totalOf.includes(SalaryType.BASIC) ? [SalaryType.BASIC, SalaryType.BASIC_INSURANCE] : body.totalOf;
     return await this.repository.create(Object.assign(body, {types}));
   }
 
@@ -25,7 +25,7 @@ export class SalarySettingsService {
   }
 
   async update(id: number, updates: UpdateSalarySettingsDto) {
-    const types = updates.totalOf.includes(SalaryType.BASIC) ? [SalaryType.BASIC, SalaryType.BASIC_INSURANCE] : updates.totalOf;
+    const types = updates?.totalOf?.length &&  updates.totalOf.includes(SalaryType.BASIC) ? [SalaryType.BASIC, SalaryType.BASIC_INSURANCE] : updates.totalOf;
     return await this.repository.update(id, Object.assign(updates, {types}));
   }
 
