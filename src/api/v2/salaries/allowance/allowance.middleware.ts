e5a3e9419eb0d3c28d7687b3b4de0bd229f0ea3d
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-05-04 21:27:53 +0700

    a

--- src/api/v2/salaries/allowance/allowance.middleware.ts
+++ src/api/v2/salaries/allowance/allowance.middleware.ts
@@ -1,6 +1,7 @@
 import {BadRequestException, Injectable, NestMiddleware} from "@nestjs/common";
 import {NextFunction} from "express";
 import {PrismaService} from "../../../../prisma.service";
+import * as moment from "moment";
 
 @Injectable()
 export class AllowanceMiddleware implements NestMiddleware {
@@ -8,25 +9,60 @@ export class AllowanceMiddleware implements NestMiddleware {
   }
 
   async use(req: Request, res: Response, next: NextFunction) {
-    await this.prisma.$use(async (params, next) => {
-      if (params.action === "createMany" || params.action === "updateMany") {
-        const data = await this.prisma.allowanceSalary.findMany({
-          where: {
-            payroll: {id: {in: (req.body as any).payrollIds}},
-            startedAt: {
-              lte: (req.body as any)?.startedAt,
+    const startedAt = new Date(moment((req.body as any)?.startedAt).set({
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }).format('YYYY-MM-DD'));
+    const endedAt = new Date(moment((req.body as any)?.endedAt).set({
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }).format('YYYY-MM-DD'));
+
+    if (req.method === "POST") {
+      const data = await this.prisma.allowanceSalary.findMany({
+        where: {
+          id: req.method === "POST" ? {notIn: (req.body as any).salaryIds} : {},
+          payroll: {id: {in: (req.body as any).payrollIds}},
+          OR: [
+            {
+              AND: [
+                {
+                  startedAt: {
+                    lte: startedAt,
+                  },
+                },
+                {
+                  endedAt: {
+                    gte: endedAt
+                  },
+                }
+              ]
             },
-            endedAt: {
-              gte: (req.body as any)?.endedAt
+            {
+              OR: [
+                {
+                  startedAt: {
+                    gte: startedAt,
+                    lte: endedAt,
+                  },
+                },
+                {
+                  endedAt: {
+                    gte: startedAt,
+                    lte: endedAt,
+                  },
+                }
+              ]
             },
-          }
-        });
-        if (data.length) {
-          throw new BadRequestException("exist");
+          ]
         }
+      });
+      if (data.length) {
+        throw new BadRequestException("Có ngày nào đó đã tồn tại rồi. Vui lòng kiểm tra lại");
       }
-      return await next(params);
-    });
+    }
     return next();
   }
 }
