b9de543a44bf6dd6be29793208c83bb0f41f3fe2
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-05-13 21:45:20 +0700

    fix build

--- src/common/dtos/mongo-id.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsMongoId } from "class-validator";
-import { ObjectId } from "mongodb";
-import { Type } from "class-transformer";
-
-export class MongoIDDto {
-  @Type(() => ObjectId)
-  @IsMongoId()
-  id: ObjectId;
-}
