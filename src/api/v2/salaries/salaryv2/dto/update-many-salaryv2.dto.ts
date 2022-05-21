b315bab412bdff7cacaa2cdfc771df482c0f5bf3
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-05-21 10:11:28 +0700

    mk

--- src/api/v2/salaries/salaryv2/dto/update-many-salaryv2.dto.ts
+++
@@ -1,9 +0,0 @@
-import {OmitType, PartialType} from "@nestjs/mapped-types";
-import {CreateSalaryv2Dto} from "./create-salaryv2.dto";
-import {IsArray, IsNumber} from "class-validator";
-
-export class UpdateManySalaryv2Dto extends OmitType(CreateSalaryv2Dto, ["payrollId"]) {
-  @IsArray()
-  @IsNumber({}, {each: true})
-  readonly salaryIds: number[];
-}
--- src/api/v2/salaries/salaryv2/dto/update-salaryv2.dto.ts
+++
@@ -1,9 +0,0 @@
-import {PartialType} from '@nestjs/mapped-types';
-import {IsArray, IsNumber} from "class-validator";
-import {CreateSalaryv2Dto} from "./create-salaryv2.dto";
-
-export class UpdateSalaryv2Dto extends PartialType(CreateSalaryv2Dto) {
-  @IsArray()
-  @IsNumber({}, {each: true})
-  readonly salaryIds: number[];
-}
