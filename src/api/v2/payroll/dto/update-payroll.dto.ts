83c4a63637927d1061b02c098603a83e0fca190f
Long Tran <90371694+LongTH20@users.noreply.github.com>
2022-05-24 13:58:42 +0700

    change lib import update payroll

--- src/api/v2/payroll/dto/update-payroll.dto.ts
+++ src/api/v2/payroll/dto/update-payroll.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreatePayrollDto } from './create-payroll.dto';
 
 export class UpdatePayrollDto extends PartialType(CreatePayrollDto) {}
